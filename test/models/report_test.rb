ed613a0f748ac93ee425a4a8d556d430eaa116ec
Paul Gonzalez <pjg5005@gmail.com>
2015-04-21 15:33:48 -0500

    Adds test coverage for reports model

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def test_accepted_report_scope
+    assert_equal(Report.accepted.count, 0)
+
+    report = reports(:first_report)
+    report.accepted_by_project_owner = true
+    report.save!
+
+    assert_equal(Report.accepted.count, 1)
+  end
+
 end
